a765c73442718d9b6fce4d20f68b42266562a0d8
Naveen Kolagatla <naveen.kolagatla@turing.com>
2022-07-25 16:26:45 -0700

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Understand how what's app availability notification affect supply quality',
+    author='turing.com',
+    license='',
+)
